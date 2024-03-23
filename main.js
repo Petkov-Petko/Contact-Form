4378d29ae713ed641acfe190b64a24bc8a9847dc
Petko Petkov <petkozlatilov1234@gmail.com>
2024-03-23 21:16:16 +0200

    updated

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-// main.js
-const form = document.querySelector('form');
+// // main.js
+// const form = document.querySelector('form');
 
-form.addEventListener('submit', async (event) => {
-  event.preventDefault();
+// form.addEventListener('submit', async (event) => {
+//   event.preventDefault();
 
-  const formData = new FormData(form);
-  formData.append('access_key', ACCESS_KEY);
+//   const formData = new FormData(form);
+//   formData.append('access_key', ACCESS_KEY);
 
-  try {
-    const response = await fetch('https://api.web3forms.com/submit', {
-      method: 'POST',
-      body: formData
-    });
+//   try {
+//     const response = await fetch('https://api.web3forms.com/submit', {
+//       method: 'POST',
+//       body: formData
+//     });
 
-    if (response.ok) {
-      console.log('Form submitted successfully!');
-      // Handle success
-    } else {
-      console.error('Form submission failed:', response.status);
-      // Handle failure
-    }
-  } catch (error) {
-    console.error('An error occurred:', error);
-    // Handle error
-  }
-});
+//     if (response.ok) {
+//       console.log('Form submitted successfully!');
+//       // Handle success
+//     } else {
+//       console.error('Form submission failed:', response.status);
+//       // Handle failure
+//     }
+//   } catch (error) {
+//     console.error('An error occurred:', error);
+//     // Handle error
+//   }
+// });
